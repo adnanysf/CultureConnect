9665dc92a0e35e6fef643ae767d678ce8c6e58a1
adnanysf <asubyusuf@gmail.com>
2024-06-27 16:19:17 -0500

    changed color layout and post structure

--- client/src/app/components/menu/Menu.tsx
+++ client/src/app/components/menu/Menu.tsx
@@ -3,21 +3,39 @@ import Link  from "next/link";
 import CIcon from '@coreui/icons-react';
 import { cilCalendar, cilUser, cilHome, cilAccountLogout} from '@coreui/icons';
 import {colors} from "../colors/Color";
+import { useState } from "react";
 
 export const title = "CultureConnect | JPMorganChase"
-
+export const Title = () => {
+    return (
+        <div style={{color:colors["Dark Blue"], fontWeight:"700"}}>{title}</div>
+    )
+}
 export const  Menu = () => {
-    const iconStyle = {
-        width: "7%",
-        color: colors["White"],
+    const [hoveredLink, setHoveredLink] = useState<String | null>("");
 
-    }
+    const iconStyle = (link: String) => ({
+        width: "7%",
+        color: colors["Off"],
+        opacity: link === hoveredLink ? "1" : "0.6",
+    });
+
     return(
-        <main className="flex w-90 items-center justify-around" style={{width:"90%", height:"8%", backgroundColor:colors["Dark Blue"], borderRadius:"2rem"}}>
-            <Link href='/' className="flex items-center justify-center h-full" style={iconStyle}><CIcon icon={cilHome}/></Link>
-            <Link href='/' className="flex items-center justify-center h-full" style={iconStyle}><CIcon icon={cilUser}></CIcon></Link>
-            <Link href='/' className="flex items-center justify-center h-full" style={iconStyle}><CIcon icon={cilCalendar}/></Link>
-            <Link href='/' className="flex items-center justify-center h-full" style={iconStyle}><CIcon icon={cilAccountLogout}/></Link>
+        <main className="flex w-90 items-center justify-around shadow" style={{width:"90%", height:"8%", backgroundColor:colors["Dark Blue"], borderRadius:"2rem", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)"}}>
+            <Link href='/feed' className="flex items-center justify-center h-full" style={iconStyle('home')} onMouseEnter={() => setHoveredLink('home')} onMouseLeave={() => setHoveredLink(null)}><CIcon icon={cilHome}/></Link>
+            <Link href='/profile' className="flex items-center justify-center h-full" style={iconStyle('user')} onMouseEnter={() => setHoveredLink('user')} onMouseLeave={() => setHoveredLink(null)}><CIcon icon={cilUser}></CIcon></Link>
+            <Link href='/' className="flex items-center justify-center h-full" style={iconStyle('calendar')} onMouseEnter={() => setHoveredLink('calendar')} onMouseLeave={() => setHoveredLink(null)}><CIcon icon={cilCalendar}/></Link>
+            <Link href='/' className="flex items-center justify-center h-full" style={iconStyle('logout')} onMouseEnter={() => setHoveredLink('logout')} onMouseLeave={() => setHoveredLink(null)}><CIcon icon={cilAccountLogout}/></Link>
         </main>
+    )
+}
+
+export const ProfilePhoto = () => {
+    return (
+        <div style={{width:"60%", height:"40%", backgroundColor:colors["Dark Blue"], borderRadius:"50%", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)",
+            justifyContent:"center", alignItems:"center", display:"flex", color:colors["White"], fontSize:"5rem", fontWeight:"700"
+        }}>
+            AY
+        </div>
     )
 }
--- client/src/app/feed/page.tsx
+++ client/src/app/feed/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from "react";
-import { Menu, title } from "../components/menu/Menu";
+import { Menu, Title } from "../components/menu/Menu";
 import {colors} from "../components/colors/Color";
 import { Filter, News } from "../components/filter/filter";
 
@@ -9,24 +9,32 @@ export default function Feed() {
     return(
         <main className="flex h-screen w-screen flex-row" style={{backgroundColor: colors["Off"]}}>
             <div className="h-full w-1/3 flex flex-col items-center p-5 gap-5">
-                <div style={{color:colors["Dark Blue"], fontWeight:"700"}}>{title}</div>
+                <Title/>
                 <Menu/>
                 <Filter/>
                 <News/>
             </div>
             <div className="h-full w-2/3 flex flex-col items-center p-5">
-                <div className="w-full h-2/6 flex items-center p-5" style={{backgroundColor:colors["Dark Blue"], borderRadius:"2rem 2rem 0 0"}}>
-                    <div className="flex flex-col items-center" style={{height:"90%", width:"80%"}}>
-                        <input placeholder="Title" className="p-4" style={{height:"20%", width:"95%", borderRadius:"1rem 1rem 0 0", borderBottom:"1px solid RGB(0,0,0,0.25)",outline:"none"}}/>
-                        <textarea placeholder="Content" className= "p-2" style={{resize:"none", height:"70%", width:"95%", borderRadius:"0 0 1rem 1rem",outline:"none"}}/>
+                {/* 212326 */}
+                <div className="w-full h-2/6 flex items-center justify-center flex-col" style={{backgroundColor:colors["Off"]}}> {}
 
-                    </div>
-                    <div>
-                        <button className="p-2 rounded-lg" style={{backgroundColor:colors["Light Blue"]}}>Post</button>
+
+                        <input placeholder="Title" className="p-4" style={{height:"10%", width:"95%", borderRadius:"1rem 1rem 0 0", 
+                        borderBottom:"1px solid RGB(0,0,0,0.25)",outline:"none", backgroundColor:colors["Off"], color:"white", border:"1px solid white"}}/>
+
+                        <textarea placeholder="Content" className= "p-4" style={{resize:"none", height:"75%", width:"95%", 
+                        borderRadius:"0 0 1rem 1rem",outline:"none",backgroundColor:colors["Off"],border:"1px solid white", color:"White"}}/>
+
+                        <div className="flex flex-row gap-3 items-center mt-1" style={{height:"25%", width:"100%"}}>
+                            <button className="ml-5 rounded-lg p-1" style={{backgroundColor:colors["Dark Blue"], color:colors["Off"]}}>Post</button>
+                            <button className="rounded-lg p-1 " style={{backgroundColor:colors["Dark Blue"], color:colors["Off"], width:"15%"}}>Add Photos</button>
+
+                        </div>
                         
-                    </div>
+                
+
                 </div>
-                <div className="w-full h-4/6 flex items-center justify-center" style={{backgroundColor:colors["Dark Blue"],borderRadius:"0 0 2rem 2rem"}}>
+                <div className="w-full h-4/6 flex items-center justify-center" style={{backgroundColor:colors["Dark Blue"],borderRadius:"2rem 2rem 2rem 2rem"}}>
                 </div>
             </div>
         </main>
