535d2df49ca8407a80044ab84d16c0c5f8c07522
adnanysf <asubyusuf@gmail.com>
2024-06-28 01:22:25 -0500

    fixed feeds page with responsive posts

--- client/src/app/components/filter/filter.tsx
+++ client/src/app/components/filter/filter.tsx
@@ -22,12 +22,18 @@ export const Filter = () => {
 export const News = () => {
     const [isExpanded, setIsExpanded] = useState(false);
 
+
+
+    const handleClick = () => {
+        setIsExpanded(!isExpanded);
+    };
     return (
         <motion.div
             className="flex items-center flex-col"
             style={{width:"100%", backgroundColor:colors["Dark Blue"], borderRadius:"2rem",boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)"}}
             initial={{ height: "5%" }}
             animate={{ height: isExpanded ? "40%" : "5%" }}
+            
         >
             <AnimatePresence>
                 {isExpanded && (
@@ -42,10 +48,18 @@ export const News = () => {
 
                         <textarea placeholder="Content" className= "p-4" style={{resize:"none", height:"60%", width:"95%", 
                             borderBottom:"1px solid RGB(255,255,255,0.5)",outline:"none",backgroundColor:colors["Dark Blue"], color:"white"}}/>
-                        <button className="rounded-lg p-1" style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"7%"}}><CIcon icon={cilSend}/></button>
-                        <button className="rounded-lg p-1 " style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"7%"}}><CIcon icon={cilFile}/></button>
-                        <button className="rounded-lg p-1 " style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"7%"}} onClick={() => setIsExpanded(!isExpanded)}><CIcon icon={cilXCircle} /></button>
+                        <div style={{width:"100%", height:"20%", display:"flex",justifyContent:"center", gap:"2rem"}}>
+                            <button className="rounded-lg p-1" style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"7%"}}><CIcon icon={cilSend}/></button>
+                            <button className="rounded-lg p-1 " style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"7%"}}><CIcon icon={cilFile}/></button>
+                            <select style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"22%", outline:"none"}}>
+                                <option value="">Select tag</option>
+                                <option value="tag1">Tag 1</option>
+                                <option value="tag2">Tag 2</option>
+                                <option value="tag3">Tag 3</option>
+                            </select>
+                            <button className="rounded-lg p-1 " style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"7%"}} onClick={() => setIsExpanded(!isExpanded)}><CIcon icon={cilXCircle} /></button>
 
+                        </div>
                     </motion.div>
                     
                 )}
@@ -56,7 +70,7 @@ export const News = () => {
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"7%", marginTop:"3px"}}
-            onClick={() => setIsExpanded(!isExpanded)}><CIcon icon={cilPlus} /></motion.button>
+            onClick={handleClick}><CIcon icon={cilPlus} /></motion.button>
             }
 
         </motion.div>
--- client/src/app/feed/page.tsx
+++ client/src/app/feed/page.tsx
@@ -5,6 +5,7 @@ import { Menu, Title } from "../components/menu/Menu";
 import {colors} from "../components/colors/Color";
 import { Filter, News } from "../components/filter/filter";
 import CIcon from '@coreui/icons-react';
+import { Post } from "../components/posts/Post";
 import { cilCalendar, cilUser, cilHome, cilAccountLogout, cilSend, cilFile} from '@coreui/icons';
 
 export default function Feed() {
@@ -17,26 +18,13 @@ export default function Feed() {
                 <Filter/>
             </div>
             <div className="h-full w-2/3 flex flex-col items-center p-5">
-                {/* style={{backgroundColor:colors["Off"]}}*/}
-                {/* <div className="w-full h-2/6 flex items-center justify-center flex-row"> 
-                    <div className="h-full flex flex-col" style={{width:"85%"}}>
+            <div className="w-full h-full flex flex-col items-center gap-10" style={{backgroundColor: colors["offW"], 
+            borderRadius: "2rem", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)", overflowY: "auto", padding:"2rem 0"}}>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
 
-                        <input placeholder="Title" className="p-4" style={{height:"10%", width:"95%", borderRadius:"1rem 1rem 0 0", 
-                        borderBottom:"1px solid RGB(0,0,0,1)",outline:"none", backgroundColor:colors["offW"], color:"black" ,boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)"}}/>
-
-                        <textarea placeholder="Content" className= "p-4" style={{resize:"none", height:"75%", width:"95%", 
-                        borderRadius:"0 0 1rem 1rem",outline:"none",backgroundColor:colors["offW"], color:"black",boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)"}}/>
-                    </div>
-                        <div className="flex flex-col gap-3 items-center mt-1 justify-center" style={{height:"100%", width:"15%"}}>
-                            <button className="rounded-lg p-1" style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"30%"}}><CIcon icon={cilSend}/></button>
-                            <button className="rounded-lg p-1 " style={{backgroundColor:colors["Dark Blue"], color:colors["offW"], width:"30%"}}><CIcon icon={cilFile}/></button>
-
-                        </div>
-                        
-                
-
-                </div> */}
-                <div className="w-full h-full flex items-center justify-center" style={{backgroundColor:colors["offW"],borderRadius:"2rem 2rem 2rem 2rem", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)"}}>
                 </div>
             </div>
         </main>
